ce3002a1f1b4f64c1815609306edb6460cd360ce
rafaapcode <rafaap2003@gmail.com>
2022-02-04 09:09:34 -0300

    Lógica completa

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Initial Data
-let a = -1;
-let b = 4;
-let c = -29;
+let a = 1;
+let b = -6;
+let c = 10;
 
 
 // Events
@@ -9,7 +9,7 @@ let c = -29;
 
 
 // Functions
-function bhaskara(){
+function bhaskara() {
 
     let b2 = b * b;
     let ac = a * c;
@@ -19,18 +19,22 @@ function bhaskara(){
     let res = b2 + four;
     let transform = res * -1;
     let result = Math.sqrt(transform);
-    
-    let x1 = bNEG /div;
-    let x1i = result / div + "i";
-    let resX1 = `${x1}${x1i}`;
-    let resX1Positivo = `${x1}+${x1i}`;
 
-    let x2 = bNEG /div;
-    let x2i = -result / div + "i";
-    let resX2 = `${x2}+${x2i}`;
-    let resX2Negativo = `${x2}${x2i}`;
-    
-   console.log(resX1, resX2);
+    let x1 = bNEG / div;
+    let x1i = result / div;
+    let resX1 = `X1= ${x1}${x1i}i,`;
+    let resX1Positivo = `X1= ${x1}+${x1i}i,`;
+
+    let x2 = bNEG / div;
+    let x2i = -result / div;
+    let resX2 = `X2= ${x2}+${x2i}i`;
+    let resX2Negativo = `X2= ${x2}${x2i}i`;
+
+    if (x1i < 0) {
+        console.log(resX1, resX2);
+    } else {
+        console.log(resX1Positivo, resX2Negativo);
+    }
 }
 
 bhaskara()
